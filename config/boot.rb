94efca781768563c10a26bc499a0c0bed92937c6
Jacob Dam <ngocphuc@gmail.com>
2010-08-22 14:23:05 +0700

    try to update boot.rb to allow rails app being runable on heroku

--- config/boot.rb
+++ config/boot.rb
@@ -1,9 +1,25 @@
+# Use locked gems if present.
+begin
+  require File.expand_path('../../.bundle/environment', __FILE__)
+
+rescue LoadError
+  # Otherwise, use RubyGems.
+  require 'rubygems'
+
+  # And set up the gems listed in the Gemfile.
+  if File.exist?(File.expand_path('../../Gemfile', __FILE__))
+    require 'bundler'
+    Bundler.setup
+  end
+end
+
+=begin
 require 'rubygems'
 
 # Set up gems listed in the Gemfile.
 gemfile = File.expand_path('../../Gemfile', __FILE__)
 begin
-  ENV['BUNDLE_GEMFILE'] = gemfile
+    ENV['BUNDLE_GEMFILE'] = gemfile
   require 'bundler'
   Bundler.setup
 rescue Bundler::GemNotFound => e
@@ -11,3 +27,4 @@ rescue Bundler::GemNotFound => e
   STDERR.puts "Try running `bundle install`."
   exit!
 end if File.exist?(gemfile)
+=end
